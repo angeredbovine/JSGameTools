92073a3a344d263b3bad91159b8b31d5b528d13d
AngeredBovine <justinmiller.math@gmail.com>
2019-05-03 16:04:25 -0400

    Basic window pane

---
+++ Build/BuildTool.sh
@@ -0,0 +1,4 @@
+DIRECTORY="./Fragments"
+OUTPUT_DIRECTORY="../"
+
+cat ${DIRECTORY}WindowPaneHeaderTop.htmlf ${DIRECTORY}WindowPaneStyles.html ${DIRECTORY}WindowPaneScripts.htmlf ${DIRECTORY}WindowPaneHeaderBottom.htmlf 
